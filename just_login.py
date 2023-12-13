603c644ffe37bc4316f44a53ff7eca3b6255e9f4
Qiyu Yan <yanqiyu17@mails.ucas.ac.cn>
2023-12-13 16:25:48 +0800

    Add Dockerfile and update just_login.py and requirements.txt

--- just_login.py
+++ just_login.py
@@ -1,8 +1,9 @@
 from xauat_login import XauatLogin
+import os
 
 if __name__ == '__main__':
-    username = 'your_mail'
-    passwd = 'your_password'
+    username = os.environ.get('UCAS_USERNAME')
+    passwd = os.environ.get('UCAS_PASSWD')
     a = XauatLogin(username=username,
                    password=passwd)
     a.log_in()
