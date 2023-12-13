9419688bb8881d316e98e0e6b5dca3f7df064431
Qiyu Yan <yanqiyu17@mails.ucas.ac.cn>
2023-12-13 16:56:27 +0800

    add debug log (dangerous as record password)

--- just_login.py
+++ just_login.py
@@ -4,6 +4,7 @@ import os
 if __name__ == '__main__':
     username = os.environ.get('UCAS_USERNAME')
     passwd = os.environ.get('UCAS_PASSWD')
+    print("login using: {username} and {passwd}".format(username=username, passwd=passwd))
     a = XauatLogin(username=username,
                    password=passwd)
     a.log_in()
